f0d97f2d97fa399810b5e63de494d6e3db8274c0
Danail Todorov <danailtodorov440@gmail.com>
2024-12-15 16:00:04 +0200

    Delete Homeworks/Glovo/Glovo directory

--- Homeworks/Glovo/Glovo/src/bg/sofia/uni/fmi/mjt/glovo/controlcenter/map/PairOfTimeAndPathPrices.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.glovo.controlcenter.map;
-
-public record PairOfTimeAndPathPrices(int estimatedTime, int price) {
-}
--- Homeworks/Glovo/Glovo/src/bg/sofia/uni/fmi/mjt/glovo/node/Node.java
+++
@@ -1,3 +0,0 @@
-package bg.sofia.uni.fmi.mjt.glovo.node;
-
-public record Node(int x, int y, int destination) { }
